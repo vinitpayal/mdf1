593b78fb14d64f546f6aabc13fa3ad21c6ebc971
vinit <vinit@holidayiq.com>
2017-07-14 20:34:08 +0530

    image upload to s3 and image crop to s3 handled

--- config.php
+++ config.php
@@ -1,29 +1,31 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://mydryfood.com/');
+define('HTTP_SERVER', 'http://local.mdf.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://mydryfood.com/');
+define('HTTPS_SERVER', 'http://local.mdf.com/');
 
 // DIR
-define('BASE_DIR','/var/www/html/mdf1/');
+define('BASE_S3_URL','https://mdf-images.s3.amazonaws.com/');
+define('BASE_DIR','/home/vinit/personal-stuff/mdf1/');
 define('DIR_APPLICATION', BASE_DIR.'catalog/');
 define('DIR_SYSTEM', BASE_DIR.'system/');
-define('DIR_IMAGE', 'https://s3.ap-south-1.amazonaws.com/mdf-images/image/');
+define('DIR_IMAGE', BASE_S3_URL.'image/');
+define('RELATIVE_IMG_DIR','image/');
 define('DIR_LANGUAGE', BASE_DIR.'catalog/language/');
-define('DIR_TEMPLATE', BASE_DIR.'mdf1/catalog/view/theme/');
-define('DIR_CONFIG', BASE_DIR.'mdf1/system/config/');
+define('DIR_TEMPLATE', BASE_DIR.'catalog/view/theme/');
+define('DIR_CONFIG', BASE_DIR.'system/config/');
 define('DIR_CACHE', BASE_DIR.'system/storage/cache/');
 define('DIR_DOWNLOAD', BASE_DIR.'system/storage/download/');
 define('DIR_LOGS', BASE_DIR.'system/storage/logs/');
 define('DIR_MODIFICATION', BASE_DIR.'system/storage/modification/');
-define('DIR_UPLOAD', 'https://s3.ap-south-1.amazonaws.com/mdf-images/images/');
+define('DIR_UPLOAD', 'https://s3.ap-south-1.amazonaws.com/mdf-images/image/');
 
 // DB
-define('DB_DRIVER', '');
-define('DB_HOSTNAME', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mydf-after-restore.cug9bmmmot7v.ap-south-1.rds.amazonaws.com');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'rootmaster123');
 define('DB_DATABASE', 'mdf');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'm_');
---
+++ system/helper/AWSConfig.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: vinit
+ * Date: 14/7/17
+ * Time: 11:04 AM
+ */
+require BASE_DIR.'vendor/autoload.php';
+use Aws\S3\S3Client;
+
+
+function getS3Configs(){
+
+    $s3ConfigArr = [];
+
+    $s3ConfigArr['bucket-name'] = 'mdf-images';
+    $s3ConfigArr['version'] = 'latest';
+    $s3ConfigArr['region'] = 'ap-south-1';
+
+    return $s3ConfigArr;
+}
+
+function getS3Client(){
+    $s3Configs = getS3Configs();
+
+    try {
+        $client = S3Client::factory(array(
+            'profile' => 'default',
+            'version' => $s3Configs['version'],
+            'region' => $s3Configs['region']
+        ));
+
+//        $s3 = new Aws\S3\S3Client([
+//            'version' => $s3Configs['version'],
+//            'region' => $s3Configs['region']
+//        ]);
+
+        return $client;
+    }
+    catch(\Exception $e) {
+        error_log("unable to get s3 client with error :".$e->getMessage());
+        return null;
+    }
+}
+
+function checkRemoteFileExistence($filePath){
+    return @getimagesize($filePath);
+}
